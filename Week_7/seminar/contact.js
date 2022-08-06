426bfbaa43839076ac1758c540eb91b3e65e2869
Nezumi <nezumi.shix@gmail.com>
2022-08-06 14:55:46 +0100

    Update contact.js

--- Week_7/seminar/contact.js
+++ Week_7/seminar/contact.js
@@ -1,5 +1,24 @@
-let subBtn=document.getElementById("subBtn");
+let subBtn = document.getElementById("subBtn");
+let cName = document.getElementById("cName");
+let cEmail = document.getElementsById("cEmail");
+let numOwl = document.getElementById("numOwl");
+let query = document.getElementById("query");
 
-subBtn.addEventListener("click", () => {
-    alert("You have successfully submitted the form!");
-})
+subBtn.addEventListener("submit", () => {
+	//alert("You have successfully submitted the form!");
+	if (
+		cName.value === "" ||
+		cEmail.value === "" ||
+		query.value === "" ||
+		numOwl.value === ""
+	) {
+		alert("You haven't filled out the form!");
+		if (cName.value === "") cName.style.background = "red";
+		if (cEmail.value === "") cEmail.style.background = "red";
+		if (numOwl.value === "") numOwl.style.background = "red";
+		if (query.value === "") query.style.background = "red";
+	} else {
+		if (numOwl.value < 1 || numOwl.value > 10)
+			alert("Number is not between 1 and 10");
+	}
+});
